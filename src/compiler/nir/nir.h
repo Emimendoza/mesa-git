sonphantrung/mesa

nir/print: Add a helper for filling src_locs
Prints the shader to a string and assigns source locations based on
that.

Reviewed-by: Adam Jackson <ajax@redhat.com>

src/compiler/nir/nir.h
@@ -4959,6 +4959,8 @@ char *nir_shader_as_str(nir_shader *nir, void *mem_ctx);
 char *nir_shader_as_str_annotated(nir_shader *nir, struct hash_table *annotations, void *mem_ctx);
 char *nir_instr_as_str(const nir_instr *instr, void *mem_ctx);
 
+char *nir_shader_gather_src_locs(nir_shader *shader, const char *filename);
+
 /** Shallow clone of a single instruction. */
 nir_instr *nir_instr_clone(nir_shader *s, const nir_instr *orig);
 

Emimendoza/mesa-git

src/compiler/nir/nir.h
@@ -4994,6 +4994,8 @@ char *nir_shader_as_str(nir_shader *nir, void *mem_ctx);
 char *nir_shader_as_str_annotated(nir_shader *nir, struct hash_table *annotations, void *mem_ctx);
 char *nir_instr_as_str(const nir_instr *instr, void *mem_ctx);
 
+char *nir_shader_gather_debug_info(nir_shader *shader, const char *filename);
+
 /** Shallow clone of a single instruction. */
 nir_instr *nir_instr_clone(nir_shader *s, const nir_instr *orig);
 